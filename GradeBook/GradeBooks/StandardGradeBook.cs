90c1ae33912ed96f3c4ee03741e0a24ddee0fea3
Michał Mistarz <galka.slawek6@gmail.com>
2024-03-13 19:58:53 +0100

    Refactor constructor of BaseGradeBook Note, once this group of tasks is begun the code will not compile until the entire group of tasks is complete.
     Add a bool to the BaseGradeBook constructor
    This should be of type bool.
    This should be the second parameter.
     Set IsWeight in the BaseGradeBook constructor
     Set the IsWeighted property using the bool parameter
     Add a bool to the StandardGradeBook constructor
    This should be of type bool.
    This should be the second parameter.
    This will require the bool to be added to the call to the base constructor.
     Add a bool to the RankedGradeBook constructor
    This should be of type bool.
    This should be the second parameter.
    This will require the bool to be added to the call to the base constructor.
     Update StartingUserInterface.CreateCommand condition
    Change the condition checking if parts is not equal to 3 to be is not equal to 4.
     Update StartingUserInterface.CreateCommand to accept IsWeighted
    This should use parts[3] for the last parameter where the gradebooks are instantiated.
    Update the message provided by this condition to write to the console "Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).".

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,7 +1,7 @@
 namespace GradeBook.GradeBooks;
 
 public class StandardGradeBook : BaseGradeBook {
-  public StandardGradeBook(string name) : base(name) {
+  public StandardGradeBook(string name, bool isWeighted) : base(name, isWeighted) {
     Type = Enums.GradeBookType.Standard;
   }
 }
--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,22 +34,23 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 3)
+            if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).\"");
                 return;
             }
             var name = parts[1];
+            bool isWeighted = bool.Parse(parts[3]);
             BaseGradeBook gradeBook;
 
             switch (parts[2].ToLower()) {
                 case "standard":
-                    gradeBook = new StandardGradeBook(name);
+                    gradeBook = new StandardGradeBook(name, isWeighted);
                     Console.WriteLine("Created gradebook {0}.", name);
                     GradeBookUserInterface.CommandLoop(gradeBook);
                     return;
                 case "ranked":
-                    gradeBook = new RankedGradeBook(name);
+                    gradeBook = new RankedGradeBook(name, isWeighted);
                     Console.WriteLine("Created gradebook {0}.", name);
                     GradeBookUserInterface.CommandLoop(gradeBook);
                     return;
